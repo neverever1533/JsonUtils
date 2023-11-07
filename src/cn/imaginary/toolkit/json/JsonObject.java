ca6bcf03ed15f0d4d762fdc7211d7a2a8fef54f8
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2023-11-07 21:28:39 +0800

    Update JsonObject.java

--- src/cn/imaginary/toolkit/json/JsonObject.java
+++ src/cn/imaginary/toolkit/json/JsonObject.java
@@ -1,6 +1,7 @@
 package cn.imaginary.toolkit.json;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
@@ -8,168 +9,127 @@ import java.util.Set;
 public class JsonObject {
 	private Properties properties_ = new Properties();
 
-	private JsonString jsonString = new JsonString();
-
 	private String closeBrace = JsonString.closeBrace;
-	private String closeBracket = JsonString.closeBracket;
 	private String colon = JsonString.colon;
 	private String comma = JsonString.comma;
 	private String null_json = JsonString.null_json;
 	private String openBrace = JsonString.openBrace;
-	private String openBracket = JsonString.openBracket;
 	private String quotation = JsonString.quotation;
 	private String space = JsonString.space;
 
-	public void add(Properties properties) {
-		update(properties, true);
+	public void add(Object key, Object value) {
+		if (null == key) {
+			key = null_json;
+		}
+		if (null == value) {
+			value = null_json;
+		}
+		properties_.put(key, value);
 	}
 
-	public void add(String key, Object value) {
-		update(key, value, false);
+	public void addAll(Map<? extends Object, ? extends Object> t) {
+		properties_.putAll(t);
+	}
+
+	public void clear() {
+		properties_.clear();
+	}
+
+	public Object clone() {
+		return properties_.clone();
+	}
+
+	public boolean containsKey(Object value) {
+		return properties_.containsKey(value);
+	}
+
+	public boolean containsValue(Object value) {
+		return properties_.containsValue(value);
 	}
 
 	public Properties get() {
 		return properties_;
 	}
 
+	public Object getValue(Object key) {
+		return properties_.get(key);
+	}
+
+	public boolean isEmpty() {
+		return properties_.isEmpty();
+	}
+
+	public void remove(Object key) {
+		properties_.remove(key);
+	}
+
+	public void replace(Object key, Object value) {
+		properties_.replace(key, value);
+	}
+
 	public void set(Properties properties) {
 		if (null != properties) {
-			properties_.clear();
-			update(properties, false);
+			clear();
+			properties_ = properties;
 		}
 	}
 
-	public void set(String key, Object value) {
-		update(key, value, true);
+	public int size() {
+		return properties_.size();
 	}
 
 	public String toStringJson() {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append(openBrace);
-		Set<Entry<Object, Object>> enSet = properties_.entrySet();
-		int p = 0;
-		Entry<Object, Object> obj;
-		Object key;
-		Object value;
-		Object[] array;
-		for (Iterator<Entry<Object, Object>> iterator = enSet.iterator(); iterator.hasNext();) {
-			obj = iterator.next();
-			if (null != obj) {
-				if (p != 0) {
-					stringBuffer.append(comma);
-					stringBuffer.append(space);
-				}
-				p++;
-				key = obj.getKey();
-				stringBuffer.append(quotation);
-				stringBuffer.append(key);
-				stringBuffer.append(quotation);
-				stringBuffer.append(colon);
-				value = obj.getValue();
-				if (null != value) {
-					if (value instanceof String) {
-						if (value.equals(null_json)) {
-							stringBuffer.append(value);
-						} else {
-							stringBuffer.append(quotation);
-							stringBuffer.append(value);
-							stringBuffer.append(quotation);
-						}
-					} else if (value instanceof JsonObject) {
-						stringBuffer.append(((JsonObject) value).toStringJson());
-					} else if (value instanceof JsonArray) {
-						stringBuffer.append(((JsonArray) value).toStringJson());
-					} else if (value instanceof Object[]) {
-						array = (Object[]) value;
-						stringBuffer.append(toStringJson(array));
-					} else {
-						stringBuffer.append(value);
-					}
-				}
-			}
-		}
-		stringBuffer.append(closeBrace);
-		return stringBuffer.toString();
+		return toStringJson(properties_);
 	}
 
-	private String toStringJson(Object[] array) {
-		StringBuffer stringBuffer;
-		if (null != array) {
-			stringBuffer = new StringBuffer();
-			stringBuffer.append(openBracket);
-			Object obj;
-			Object[] arr;
-			for (int i = 0, len = array.length; i < len; i++) {
-				obj = array[i];
-				if (null != obj) {
-					if (i != 0) {
-						stringBuffer.append(comma);
-						stringBuffer.append(space);
-					}
-					if (obj instanceof String) {
-						if (obj.equals(null_json)) {
-							stringBuffer.append(obj);
-						} else {
-							stringBuffer.append(quotation);
-							stringBuffer.append(obj);
-							stringBuffer.append(quotation);
-						}
-					} else if (obj instanceof JsonObject) {
-						stringBuffer.append(((JsonObject) obj).toStringJson());
-					} else if (obj instanceof JsonArray) {
-						stringBuffer.append(((JsonArray) obj).toStringJson());
-					} else if (obj instanceof Object[]) {
-						arr = (Object[]) obj;
-						stringBuffer.append(toStringJson(arr));
-					} else {
-						stringBuffer.append(obj);
-					}
-				}
-			}
-			stringBuffer.append(closeBracket);
-			return stringBuffer.toString();
-		}
-		return null;
-	}
-
-	private void update(Properties properties, boolean isReplace) {
+	public String toStringJson(Properties properties) {
 		if (null != properties) {
+			StringBuffer stringBuffer = new StringBuffer();
+			stringBuffer.append(openBrace);
 			Set<Entry<Object, Object>> enSet = properties_.entrySet();
+			int p = 0;
+			Entry<Object, Object> entry;
 			Object key;
 			Object value;
-			Entry<Object, Object> entry;
 			for (Iterator<Entry<Object, Object>> iterator = enSet.iterator(); iterator.hasNext();) {
 				entry = iterator.next();
 				if (null != entry) {
+					if (p != 0) {
+						stringBuffer.append(comma);
+						stringBuffer.append(space);
+					}
+					p++;
 					key = entry.getKey();
+					stringBuffer.append(quotation);
+					stringBuffer.append(key);
+					stringBuffer.append(quotation);
+					stringBuffer.append(colon);
 					value = entry.getValue();
-					if (null != key && null != value) {
-						update(key.toString(), value, isReplace);
+					if (null != value) {
+						if (value instanceof String) {
+							if (value.equals(null_json)) {
+								stringBuffer.append(value);
+							} else {
+								stringBuffer.append(quotation);
+								stringBuffer.append(value);
+								stringBuffer.append(quotation);
+							}
+						} else if (value instanceof JsonObject) {
+							stringBuffer.append(((JsonObject) value).toStringJson());
+						} else if (value instanceof JsonArray) {
+							stringBuffer.append(((JsonArray) value).toStringJson());
+						} else if (value instanceof Object[]) {
+							stringBuffer.append(JsonArray.toStringJson((Object[]) value));
+						} else {
+							stringBuffer.append(value);
+						}
 					}
 				}
 			}
+			stringBuffer.append(closeBrace);
+			return stringBuffer.toString();
 		}
-	}
-
-	private void update(String key, Object value, boolean isReplace) {
-		if (null != key) {
-			if (null == value) {
-				value = null_json;
-			}
-			key = jsonString.toStringJson(key);
-			if (value instanceof String) {
-				value = jsonString.toStringJson(value.toString());
-			}
-			if (isReplace) {
-				if (properties_.containsKey(key)) {
-					properties_.replace(key, value);
-				}
-			} else {
-				if (!properties_.containsKey(key)) {
-					properties_.put(key, value);
-				}
-			}
-		}
+		return null;
 	}
 
 }
